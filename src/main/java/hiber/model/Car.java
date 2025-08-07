545e4b9027a01f34854c6db36b5e0879a8a9db69
Dinicels <asitack15@gmail.com>
2025-08-07 14:18:37 +0300

    v1

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,49 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String model;
+    @Column
+    private int series;
+
+    public Car(String model, int series) {
+        this.id = id;
+        this.model = model;
+        this.series = series;
+    }
+
+    public Car() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
